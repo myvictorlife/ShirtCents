835826fa60e6b344d4b99db1d969bb7c6545f423
pedrohenriquemartins <ph_gaara@hotmail.com>
2014-11-11 20:11:45 -0200

    Commit Pedro

--- ShirtCents/src/java/Bean/UsuarioBean.java
+++ ShirtCents/src/java/Bean/UsuarioBean.java
@@ -11,7 +11,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import util.JpaUtil;
 
-
+//Commit Pedro
 @ManagedBean
 @SessionScoped
 public class UsuarioBean {
