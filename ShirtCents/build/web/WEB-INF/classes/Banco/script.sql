5673edb77e07e92efa0f64c4f449c00ecbbefbec
victorcpeixoto <victorcmggg@gmail.com>
2014-11-23 13:05:13 -0200

    Commit Victor  11/11

--- ShirtCents/build/web/WEB-INF/classes/Banco/script.sql
+++ ShirtCents/build/web/WEB-INF/classes/Banco/script.sql
@@ -1,14 +1,118 @@
 
-CREATE TABLE USUARIO (
+--------- CRIANDO BANCO DE DADOS ------
+-- NOME DO BANCO DE DADOS:  shirtCents
+-- NOME DO USUARIO: shirt
+-- SENHA DO BANCO DE DADOS: cents
+-- -------------------------------------
 
-    ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-    NOME VARCHAR(45) NOT NULL,
-    EMAIL VARCHAR(45),
-    LOGIN VARCHAR(30) NOT NULL,
-    SENHA VARCHAR(30) NOT NULL,
-    PROFILE VARCHAR(10) NOT NULL,
-    PRIMARY KEY (ID)
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Cliente`
+-- -----------------------------------------------------
+
+CREATE TABLE Cliente (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  nome VARCHAR(45),
+  telefone VARCHAR(45),
+  cpf VARCHAR(45),
+  rg VARCHAR(45),
+  email VARCHAR(45),
+  PRIMARY KEY (id)
 );
 
-INSERT INTO USUARIO (NOME, EMAIL, LOGIN, SENHA, PROFILE)
-    VALUES ('Victor Cesar', 'victorcmggg@gmail.com', 'victor', '123', 'Admin');
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Endereco`
+-- -----------------------------------------------------
+
+CREATE TABLE Endereco (
+  idEndereco INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  logradouro VARCHAR(45),
+  numero VARCHAR(45),
+  complemento VARCHAR(45),
+  cidade VARCHAR(45),
+  estado VARCHAR(45),
+  pais VARCHAR(45),
+  cep VARCHAR(45),
+  Cliente_id_cliente INT NOT NULL,
+  PRIMARY KEY (idEndereco, Cliente_id_cliente),
+  FOREIGN KEY (Cliente_id_cliente) REFERENCES Cliente (id)
+  );
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Categoria`
+-- -----------------------------------------------------
+
+CREATE TABLE Categoria (
+  id_categoria INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  categoria VARCHAR(45),
+  PRIMARY KEY (id_categoria));
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Produto`
+-- -----------------------------------------------------
+
+CREATE TABLE Produto (
+  id_produto INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  descricao VARCHAR(45),
+  tamanho VARCHAR(10),
+  preco_custo DOUBLE,
+  quantidade INT,
+  id_categoria INT NOT NULL,
+  PRIMARY KEY (id_produto),
+  FOREIGN KEY (id_categoria) REFERENCES Categoria (id_categoria)
+   );
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`FormaPagamento`
+-- -----------------------------------------------------
+
+CREATE TABLE FormaPagamento (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  descricao VARCHAR(45),
+  PRIMARY KEY (id)
+);
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Pedido`
+-- -----------------------------------------------------
+
+CREATE TABLE Pedido (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  pagamento_pedido DOUBLE,
+  status_pedido VARCHAR(50),
+  criado_pedido DATE,
+  modificado_pedido DATE,
+  Cliente_id_cliente INT NOT NULL,
+  FormaPagamento_id INT NOT NULL,
+  PRIMARY KEY (id, Cliente_id_cliente, FormaPagamento_id),
+  FOREIGN KEY (Cliente_id_cliente) REFERENCES Cliente (id),
+  FOREIGN KEY (FormaPagamento_id) REFERENCES FormaPagamento (id)
+ );
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Itens`
+-- -----------------------------------------------------
+
+--CREATE TABLE Itens (
+--  quantidade DOUBLE,
+--   valor_unitario DOUBLE,
+--   id_produto INT NOT NULL,
+-- Pedido_id INT NOT NULL,
+--   PRIMARY KEY (id_produto, Pedido_id),
+--   FOREIGN KEY (id_produto) REFERENCES Produto (id_produto),
+--   FOREIGN KEY (Pedido_id) REFERENCES Pedido (id)
+-- );
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`usuario`
+-- -----------------------------------------------------
+
+CREATE TABLE usuario (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  nome VARCHAR(45),
+  email VARCHAR(45),
+  login VARCHAR(30),
+  senha VARCHAR(30),
+  profile VARCHAR(10),
+  PRIMARY KEY (id)
+);
+
---
+++ ShirtCents/src/java/Banco/comandos.sql
@@ -0,0 +1,70 @@
+--INSERT INTO TABLE () VALUES ();
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Cliente`
+-- -----------------------------------------------------
+
+insert into Cliente (nome, telefone, cpf, rg, email) 
+   values ('Maria Gabriela', '8888-8888', '016402245-07', '5483330','maria@yahoo.com.br');
+
+insert into Cliente (nome, telefone, cpf, rg, email) 
+   values ('Joao Carlos', '1111-1111', '352784357-08', '3256845','joaoc@gmail.com');
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Endereco`
+-- -----------------------------------------------------
+
+INSERT INTO Endereco (logradouro, numero, complemento, cidade, estado, pais, cep, Cliente_id_cliente) 
+   VALUES ('Avenida Amazonas', '1330', 'apart 222', 'Uberlandia', 'MG', 'Brasil', '38777-440', 1);
+
+INSERT INTO Endereco (logradouro, numero, complemento, cidade, estado, pais, cep, Cliente_id_cliente) 
+   VALUES ('Rua Abadia', '330', '', 'Araguari', 'MG', 'Brasil', '38444-440', 2);
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Categoria`
+-- -----------------------------------------------------
+
+INSERT INTO categoria (categoria) VALUES ('CAMISA');
+INSERT INTO categoria (categoria) VALUES ('PULSEIRA');
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Produto`
+-- -----------------------------------------------------
+
+INSERT INTO Produto (descricao, tamanho, preco_custo, quantidade, id_categoria) 
+    VALUES ('Camisa Polo', 'G', 22.90, 20, 1);
+
+INSERT INTO Produto (descricao, tamanho, preco_custo, quantidade, id_categoria) 
+    VALUES ('Pandora', 'A', 12.90, 12, 2);
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`FormaPagamento`
+-- -----------------------------------------------------
+
+INSERT INTO FormaPagamento (descricao) VALUES('Cartao');
+INSERT INTO FormaPagamento (descricao) VALUES('Dinheiro');
+INSERT INTO FormaPagamento (descricao) VALUES('Cheque');
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Pedido`
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`Itens`
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Table `shirtCents`.`usuario`
+-- -----------------------------------------------------
+
+INSERT INTO USUARIO (NOME, EMAIL, LOGIN, SENHA, PROFILE)
+    VALUES ('Victor Cesar', 'victorcmggg@gmail.com', 'victor', '123', 'Admin');
+
+INSERT INTO USUARIO (NOME, EMAIL, LOGIN, SENHA, PROFILE)
+    VALUES ('Gilberto Olimpio', 'gilberto.olimpio@gmail.com', 'gilbert', '321', 'Admin');
+
+INSERT INTO USUARIO (NOME, EMAIL, LOGIN, SENHA, PROFILE)
+    VALUES ('Pablo Ribeiro', 'pableeenho@gmail.com', 'pablo', '123', 'Leitor');
+
+
+
+
