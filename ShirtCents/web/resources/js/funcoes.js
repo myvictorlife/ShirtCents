a0e7f7d78235727eb6467b9892813708a71498a9
victorcpeixoto <victorcmggg@gmail.com>
2014-11-24 19:37:19 -0200

    Commit Victor  11/11

--- ShirtCents/web/resources/js/funcoes.js
+++ ShirtCents/web/resources/js/funcoes.js
@@ -4,3 +4,14 @@ function verifica() {
     }
     return false;
 }
+
+//Login
+    function handleLoginRequest(xhr, status, args) {
+        if(args.validationFailed || !args.loggedIn) {
+            PF('dlg').jq.effect("shake", {times:5}, 100);
+        }
+        else {
+            PF('dlg').hide();
+            $('#loginLink').fadeOut();
+        }
+    }
